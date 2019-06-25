fc0f7985f93e6c8b5091532179996e5f5e7be948
Ishan Shah <ishanshah02@gmail.com>
2019-06-24 22:02:51 -0400

    Test Deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,17 @@
 module.exports = {
   siteMetadata: {
     title: 'CR Coding',
+    description: 'Free Coding Lessons',
+    keywords: 'Coding, kickstart, crcoding, gulliver preparatory, gulliver academy'
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: 'msgwwgrjg3be',
+        accessToken: '0VXMvj82TZrG4n4SEolRhKACDhifDuOXf13L33w2qb8'
+      }
+    }
+  ],
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+// import React from 'react'
+// import styled from 'styled-components'
+
+
+
+// const Footer = ({data}) => {
+//   {data.allContentfulLink.edges.map(edge => (
+//     <a href={edge.node.url}>{edge.node.title}</a>
+// ))} 
+
+// }
+
+// export default Footer
